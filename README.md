127bede093823f49793c7e4cd2fc1e8e2b13a492
mffonua <108596346+mffonua@users.noreply.github.com>
2022-08-11 23:13:39 -0600

    Added in questions from w3 schools as well as finished readme.md

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Timed-JavaScript-Quiz
+#### Timed-JavaScript-Quiz
+
+### By _**{Miles Fonua}**_
+
+CONTENTS OF THIS FILE
+---------------------
+
+ * Introduction
+ * Deployment
+ * Technology Used
+ * Installation
+ * Known Bugs/ Creator Content
+
+ ### Introduction
+ The Timed JavaScript Quiz is a timed quiz that challenges the user based of their knowledge of JavaScript (Quiz Based of w3 Schools). To start this quiz the User must click on the "Start Quiz" button. The User will then go through a series of multiple choice questions, Each incorrect answer will result in 10 seconds be deducted from the timer. 
+
+ Once the user runs out of time or finishes the quiz, the timer will stop. The user will either have a choice to enter their initials in the HighScore, or Play again. If opted to play again the quiz will loop back to the beginnig for the user to try again.
+
+ ### Deployment
+
+ https://mffonua.github.io/Timed-JavaScript-Quiz/
+
+
+ ![Screenshot of Quiz](./assets/images/screenshot.jpg)
+
+## Technologies Used
+
+* _My apple-mac laptop_
+~Website Resources~
+* W3 Schools
+* Lots of Google
+* Lots of Youtube
+
+## Installation
+
+ * To install the user must have access to the Internet; Once connected to the internet the user must enter 
+ in the url https://mffonua.github.io/Timed-JavaScript-Quiz . After clicking enter the user is then led to the JavaScript Timed quiz and is able to use the website.
+
+ ## Known Bugs & Creator Notes:
+
+ * This quiz was a complete pain and I have spent countless nights as well as many resets of this web application.
+ The final app you see is a result of 3 complete resets and many trial and errors.
+ * Known Bugs : NONE 
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -267,3 +267,215 @@ godMode.addEventListener("click", function () {
         startQuiz();
     }
 }) 
+
+//The following Quiz Questions are from the W3 Schools java script quiz
+var questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            { choice: "<js>", correct: false },
+            { choice: "<script>", correct: true },
+            { choice: "<scripting>", correct: false },
+            { choice: "<javascript>", correct: false }
+        ]
+    },
+    {
+        question: "What is the correct JavaScript syntax to change the content of the HTML element below?<br>&lt;p id='demo'&gt;This is a demonstration.&lt;/p&gt;",
+        answers: [
+            { choice: "document.getElementById('demo').innerHTML = 'Hello World!';", correct: true },
+            { choice: "document.getElement('p').innerHTML = 'Hello World!';", correct: false },
+            { choice: "document.getElementByName('p').innerHTML = 'Hello World!';", correct: false },
+            { choice: "#demo.innerHTML = 'Hello World!';", correct: false }
+        ]
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        answers: [
+            { choice: "The <body> section", correct: false },
+            { choice: "The <head> section", correct: false },
+            { choice: "Both the <head> section and the <body> section are correct", correct: true },
+            { choice: "It connects automatically to the HTML document", correct: false }
+        ]
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        answers: [
+            { choice: "<script src='xxx.js'>", correct: true },
+            { choice: "<script href='xxx.js'>", correct: false },
+            { choice: "<script name='xxx.js'>", correct: false },
+            { choice: "<source script='xxx.js'>", correct: false }
+        ]
+    },
+    {
+        question: "The external JavaScript file must contain the &ltscript&gt tag.",
+        answers: [
+            { choice: "True", correct: false },
+            { choice: "False", correct: true },
+            { choice: "invisible", correct: false },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: [
+            { choice: "alert('Hello World');", correct: true },
+            { choice: "msg('Hello World');", correct: false },
+            { choice: "msgBox('Hello World');", correct: false },
+            { choice: "alertBox('Hello World');", correct: false }
+        ]
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: [
+            { choice: "function myFunction()", correct: true },
+            { choice: "function:myFunction()", correct: false },
+            { choice: "function = myFunction()", correct: false },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        answers: [
+            { choice: "'This is a comment", correct: false },
+            { choice: "//This is a comment", correct: true },
+            { choice: "<!--This is a comment-->", correct: false },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "How to insert a comment that has more than one line?",
+        answers: [
+            { choice: "//This comment has<br>more than one line//", correct: false },
+            { choice: "<!--This comment has<br>more than one line-->", correct: false },
+            { choice: "/*This comment has<br>more than one line*/", correct: true },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        answers: [
+            { choice: "var colors = (1:'red', 2:'green', 3:'blue')", correct: false },
+            { choice: "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')", correct: false },
+            { choice: "var colors = 'red', 'green', 'blue'", correct: false },
+            { choice: "var colors = ['red', 'green', 'blue']", correct: true }
+        ]
+    },
+    {
+        question: "How do you round the number 7.25, to the nearest integer?",
+        answers: [
+            { choice: "Math.round(7.25)", correct: true },
+            { choice: "round(7.25)", correct: false },
+            { choice: "rnd(7.25)", correct: false },
+            { choice: "Math.rnd(7.25)", correct: false }
+        ]
+    },
+    {
+        question: "What is the correct JavaScript syntax for opening a new window called 'w2' ?",
+        answers: [
+            { choice: "w2 = window.new('http://www.w3schools.com');", correct: false },
+            { choice: "w2 = window.open('http://www.w3schools.com');", correct: true },
+            { choice: "invisible", correct: false },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "JavaScript is the same as Java.",
+        answers: [
+            { choice: "True", correct: false },
+            { choice: "False", correct: true },
+            { choice: "invisible", correct: false },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "How can you detect the client's browser name?",
+        answers: [
+            { choice: "browser.name", correct: false },
+            { choice: "client.navName", correct: false },
+            { choice: "navigator.appName", correct: true },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        answers: [
+            { choice: "onmouseclick", correct: false },
+            { choice: "onchange", correct: false },
+            { choice: "onmouseover", correct: false },
+            { choice: "onclick", correct: true }
+        ]
+    },
+    {
+        question: "How do you declare a JavaScript variable?",
+        answers: [
+            { choice: "variable carName;", correct: false },
+            { choice: "var carName;", correct: true },
+            { choice: "v carName;", correct: false },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        answers: [
+            { choice: "=", correct: true },
+            { choice: "x", correct: false },
+            { choice: "-", correct: false },
+            { choice: "*", correct: false }
+        ]
+    },
+    {
+        question: "What will the following code return: Boolean(10 > 9)",
+        answers: [
+            { choice: "true", correct: true },
+            { choice: "false", correct: false },
+            { choice: "NaN", correct: false },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "Is JavaScript case-sensitive?",
+        answers: [
+            { choice: "Yes", correct: true },
+            { choice: "No", correct: false },
+            { choice: "invisible", correct: false },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "How do you call a function named 'myFunction'?",
+        answers: [
+            { choice: "myFunction()", correct: true },
+            { choice: "call myFunction()", correct: false },
+            { choice: "call function myFunction()", correct: false },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        answers: [
+            { choice: "if (i == 5)", correct: true },
+            { choice: "if i = 5 then", correct: false },
+            { choice: "if i == 5 then", correct: false },
+            { choice: "if i = 5", correct: false }
+        ]
+    },
+    {
+        question: "How does a WHILE loop start?",
+        answers: [
+            { choice: "while (i <= 10; i++)", correct: false },
+            { choice: "while (i <= 10)", correct: true },
+            { choice: "while i = 1 to 10", correct: false },
+            { choice: "invisible", correct: false }
+        ]
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: [
+            { choice: "for (i <= 5; i++)", correct: false },
+            { choice: "for i = 1 to 5", correct: false },
+            { choice: "for (i = 0; i <= 5; i++)", correct: true },
+            { choice: "for (i = 0; i <= 5)", correct: false }
+        ]
+    }
+
+]
