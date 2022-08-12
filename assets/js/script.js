a34979397e67c2c5a844f9c7065447aa2d741d06
mffonua <108596346+mffonua@users.noreply.github.com>
2022-08-11 21:06:52 -0600

    adding in more of old html

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var gameContainerEl = document.querySelector("#game-main");
+var highScoresPEl = document.querySelector("#check-high-scores");
+var yourScoreEl = document.querySelector("#your-score");
+var timeEl = document.querySelector("#time-remaining");
